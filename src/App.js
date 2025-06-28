f142f7234d6a6761284a295c3fac80e228a008d6
DeepakTayde <deepaktayde559@gmail.com>
2025-06-28 20:55:27 +0530

    timer component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,41 @@
-
-import './App.css';
+import { useRef, useState } from "react";
+import "./App.css";
 
 function App() {
+  const [timer, setTimer] = useState(0);
+  const [isRunning, setIsRunning] = useState(false);
+  const intervalRef = useRef(null);
+
+  const handleStartStop = () => {
+    if (isRunning) {
+      clearInterval(intervalRef.current);
+      setIsRunning(false);
+    } else {
+      intervalRef.current = setInterval(() => {
+        setTimer((prev) => prev + 1);
+      }, 1000);
+      setIsRunning(true);
+    }
+  };
+
+  const handleReset = () => {
+    clearInterval(intervalRef.current);
+    setTimer(0);
+    setIsRunning(false);
+  };
   return (
     <div className="App">
       <header className="App-header">
-          <h2>Stopwatch</h2>
+        <h2>Stopwatch</h2>
+        <h3>Time: {timer}</h3>
+        <div>
+          <button type="button" onClick={handleStartStop}>
+            {isRunning ? "Stop" : "Start"}
+          </button>
+          <button type="button" onClick={handleReset}>
+            Reset
+          </button>
+        </div>
       </header>
     </div>
   );
